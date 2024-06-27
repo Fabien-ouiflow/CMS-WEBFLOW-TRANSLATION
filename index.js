d4c6957ea15afe6c7704118b160c101e6321435c
AlexTznDev <alex.tuysuzian91@gmail.com>
2024-06-27 13:29:20 +0200

    Add process text end point

--- index.js
+++ index.js
@@ -5,26 +5,36 @@ const PORT = process.env.PORT || 3000;
 // Middleware pour parser le body en JSON
 app.use(express.json());
 
+// Fonction pour traiter la chaîne de caractères
+function processString(data) {
+    let result = data.replace(/"/g, '\\"');  // Ajoute un antislash avant chaque guillemet double
+    result = result.replace(/'/g, ' ');      // Remplace toutes les apostrophes par un espace
+    return result;
+}
+
+// Endpoint pour modifier du HTML selon les règles spécifiées
 app.post('/modify-html', (req, res) => {
     if (req.body.translations && req.body.translations.length > 0 && req.body.translations[0].text) {
-        // Extraire le HTML du champ "text"
         let htmlContent = req.body.translations[0].text;
-
-        // Remplacer chaque apostrophe par un espace
         htmlContent = htmlContent.replace(/'/g, ' ');
-
-        // Convertir le HTML modifié en JSON pour échapper automatiquement les guillemets
         let jsonResponse = JSON.stringify(htmlContent);
-
-        // Envoyer le HTML échappé encapsulé dans un objet JSON
         res.send(jsonResponse);
     } else {
-        // Envoyer une réponse d'erreur si les données nécessaires ne sont pas présentes
         res.status(400).send('Invalid request data');
     }
 });
 
+// Nouvel endpoint pour traiter la chaîne de caractères selon processString
+app.post('/process-text', (req, res) => {
+    if (req.body.translations && req.body.translations.length > 0 && req.body.translations[0].text) {
+        const processedText = processString(req.body.translations[0].text);
+        res.json({ processedText });
+    } else {
+        res.status(400).send('Invalid request data');
+    }
+});
 
+// Démarrage du serveur
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
