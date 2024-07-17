d348181569784e1302a34ce151304d31b72cb4d5
AlexTznDev <alex.tuysuzian91@gmail.com>
2024-07-17 11:37:42 +0200

    modification &#x27;

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -6,45 +6,50 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 function processString(data) {
-    let result = data.replace(/'/g, ' ');       // Remplace toutes les apostrophes par un espace
-    result = result.replace(/"/g, '\\"');       // Ajoute un antislash avant chaque guillemet double
-    return result;
+  let result = data.replace(/'/g, "&#x27;"); 
+  result = result.replace(/"/g, '\\"'); 
+  return result;
 }
 
-// Conserver l'endpoint '/modify-html' comme spécifié
-app.post('/modify-html', (req, res) => {
-    if (req.body.translations && req.body.translations.length > 0 && req.body.translations[0].text) {
-        let htmlContent = req.body.translations[0].text;
-        htmlContent = htmlContent.replace(/'/g, ' ');
-        let jsonResponse = JSON.stringify(htmlContent);
-        res.send(jsonResponse);
+
+app.post("/modify-html", (req, res) => {
+
+    if (req.body === 0) {
+      res.send("0"); 
+    } else if (
+      req.body.translations &&
+      req.body.translations.length > 0 &&
+      req.body.translations[0].text
+    ) {
+      let htmlContent = req.body.translations[0].text;
+      htmlContent = htmlContent.replace(/'/g, "&#x27;");
+      let jsonResponse = JSON.stringify(htmlContent);
+      res.send(jsonResponse);
     } else {
-        res.status(400).send('Invalid request data');
+      res.status(400).send("Invalid request data");
     }
-});
+  });
+  
 
 // Modifier l'endpoint '/process-text' pour accepter du texte brut
-app.post('/process-text', express.text(), (req, res) => {
-    console.log(req.body);  // Pour déboguer et voir ce que le serveur reçoit
+app.post("/process-text", express.text(), (req, res) => {
+  console.log(req.body); 
 
-    // Vérifie si req.body est exactement le chiffre 0
-    if (req.body === '0') {
-        res.send('0');  // Retourne juste un espace
-    }
-    // Vérifie que req.body est une chaîne et non vide
-    else if (typeof req.body === 'string' && req.body.trim() !== '') {
-        const processedText = processString(req.body);
-        res.send(processedText);
-    } else {
-        // Renvoie un message personnalisé si le corps de la requête est vide
-        res.status(400).send('Le contenu de cette ligne CMS est vide.');
-    }
+
+  if (req.body === "0") {
+    res.send("0"); 
+  }
+
+  else if (typeof req.body === "string" && req.body.trim() !== "") {
+    const processedText = processString(req.body);
+    res.send(processedText);
+  } else {
+
+    res.status(400).send("Le contenu de cette ligne CMS est vide.");
+  }
 });
-
-
-
 
 // Démarrage du serveur
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
