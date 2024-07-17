7401f68af38417aa6e225ecc87a0120395830348
AlexTznDev <alex.tuysuzian91@gmail.com>
2024-07-17 13:12:00 +0200

    modification

--- index.js
+++ index.js
@@ -6,38 +6,45 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 function processString(data) {
-  let result = data.replace(/'/g, "&#x27;");
-  result = result.replace(/"/g, '\\"');
+  let result = data.replace(/'/g, "&#x27;"); 
+  result = result.replace(/"/g, '\\"'); 
   return result;
 }
 
+
 app.post("/modify-html", (req, res) => {
-  if (req.body === 0) {
-    res.send("0");
-  } else if (
-    req.body.translations &&
-    req.body.translations.length > 0 &&
-    req.body.translations[0].text
-  ) {
-    let htmlContent = req.body.translations[0].text;
-    htmlContent = processString(htmlContent); // Utilisation de processString ici
-    let jsonResponse = JSON.stringify(htmlContent);
-    res.send(jsonResponse);
-  } else {
-    res.status(400).send("Invalid request data");
-  }
-});
+
+    if (req.body === 0) {
+      res.send("0"); 
+    } else if (
+      req.body.translations &&
+      req.body.translations.length > 0 &&
+      req.body.translations[0].text
+    ) {
+      let htmlContent = req.body.translations[0].text;
+      htmlContent = htmlContent.replace(/'/g, "&#x27;");
+      let jsonResponse = JSON.stringify(htmlContent);
+      res.send(jsonResponse);
+    } else {
+      res.status(400).send("Invalid request data");
+    }
+  });
+  
 
 // Modifier l'endpoint '/process-text' pour accepter du texte brut
 app.post("/process-text", express.text(), (req, res) => {
-  console.log(req.body);
+  console.log(req.body); 
+
 
   if (req.body === "0") {
-    res.send("0");
-  } else if (typeof req.body === "string" && req.body.trim() !== "") {
+    res.send("0"); 
+  }
+
+  else if (typeof req.body === "string" && req.body.trim() !== "") {
     const processedText = processString(req.body);
     res.send(processedText);
   } else {
+
     res.status(400).send("Le contenu de cette ligne CMS est vide.");
   }
 });
